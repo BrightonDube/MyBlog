9629f3248a9124389abb9c3c4cd38a082f06a58b
Brighton <BrightonDube@users.noreply.github.com>
2025-04-06 09:54:24 +0200

    `Refactor Post model: rename 'body' field to 'content'`

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  body: { type: String, required: true },
+  content: { type: String, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
 });
